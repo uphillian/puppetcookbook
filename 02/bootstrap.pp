c853aa1ff4aed1ec408b64f6a5206e8c20df4754
Thomas Uphill <thomas@narrabilis.com>
2018-01-18 14:21:55 -0800

    bootstrap updated for 5

--- 02/bootstrap.pp
+++ 02/bootstrap.pp
@@ -1,88 +1,119 @@
 package {'git':
   ensure => 'installed'
 }
+
 File {
-  owner => 'puppet',
-  group => 'puppet',
-}
-file {'/var/lib/puppet/.ssh':
-  ensure => 'directory',
-}
-file {'/var/lib/puppet/.ssh/id_rsa':
-  content => "
------BEGIN RSA PRIVATE KEY-----
-MIIJJgIBAAKCAgEAwfqUB0eU+oPRzlZrBM1KDBjSLjK5bgrHUF9x5zjhHgpC0o/y
-LaCV5rTl/FiVtbsxuCTHIHUZhLNlTpZ4/x8RMaP2yEk53zuS2v8UoG7Fyon06gGx
-QID5FmmSA9365Gpo/z54MzwDemD6JseMkI6UOhymsbyftCt+usJcejM7NXWO7T+7
-6a7RPMficY8Z39E0+De63+QiBOo8HzU0wtfSvR4q8U1im7OgTR71iTK8DQpD1WhK
-3A+4xUplytaCfRdn4DHhZ2euhR1Fy7xTg1LBwqKCQZxDPgv3uEEUcfnzU18ty532
-DM7XtcS8oOpaZatdSp9eV97R9O8CZ7IzovN3wG1gjg1EDK0GqdraRjNFpDOh6uYO
-c9Vtbi/n9GDHFDVxDdV5IHthpBYa1YugCWnLswtCTTO+uP7cOKyUVxGGmyXzT+FA
-OmLt9mav6ijHHn+YHjai0pKVmxsqaqR0hnS92hwhiXktNG4Sz9D7a0oy373tTxKC
-dYl29DlX00jOZ5xLpCeMkCwmYZmCHAsKobBrsltRK1anzzC713/l+MROEohYzF5z
-bcw0QpBhSElkzcB229BdDIHcpHdN4J3fryWnrKV07PrL+EPVpEtcAXeB7hnQGtUM
-Q9ijl9AnAIc+zIqVaO63n6x7n7j+M5E2P3bwqYapTmpb8fjALspjXLTfrBsCASMC
-ggIAFitEHhbPMpK45GGl1Km/Udbzcv57l4w0CS96u1b1KAEsNVJHkC+cGl3RJCdh
-kRy1OZ3NnU82HctcCPtBBzbAIu4q1Q+u168JeB0m7cOEUald0ZnLHVCRfud+ZteK
-Yz9cdPEyTv+LXnF7rKkXYPpaFUUaXXSkiZ6SI/j0oEBeicRK2Ukcyj/dZgg3IuvI
-cVm86SrwySFi+T9XVAYUqI2xrzal0m8+eDHJLWKft+iKhSW+nAvrTFmRdaIaQxEz
-fAKsySpFpWrvX6uS8rZv8cBQqIeZ25zUfB6f98zsZMwbzwOQNIcUzkOGXeqZNvY2
-N4FMfY6OcHE8kQVfXE7hVHOYqBQLYMTQavqdSYL18of77qVt3CKjk7ERjU7+gFLE
-pl4LH9DjVWp5L7XTZ41VoY0cnvT52fL426VZhYUb6jFsEPal+eJHjD6UgmJbQsa2
-7iMrjT/yERdrJyfpyOmSNx+foEPaOVie7AfDQe3WWd4KTQ4zkOZ7TKh9XwO2cuQL
-jNLnSeUKVJaZPNXLuoeTiIiKsw41+FVrw/3m3Q84xTqfdjXyQlpLXkT3EgD5d4wl
-NoQLIXjbgJRP4yLgwD54rurJcEJxu5zkeGR4X436QSgfGUDkDNmP721qWqe7mjpv
-ItoTQR+sb+7AXwSnnQHVb7ynaTCDaZaBK0Jzm/S7wfvLjQsCggEBAOc6hMmDICGT
-U1m/i01i/I0cCSC5XJgPAFNY9HmyCJKEuUURwHvECjGkF/qr6flp83jt5g0ZDvv4
-PntoC79Ropg5qa4ahgafbOwGmJtH70rQZ3lUDQOsMjRjpxpfZI/sSheLv5zVjJ+P
-iroVdlwSVfdJrvmR7XujNf3M/SOZbsX+KQm7USz/dYljLkyGSLyjkakZZWhU/813
-MORVTVJ0wQR5bBNpFohrKF2xUtiebKqpwEnpWs0aS3//EouooOYnHytw9za5WlaR
-K8nTyg/nYurRC25cf5D7It65rrsPxxNbVl7ZkRfqdnzKXnVl2drcL7TK3o4ZgFKi
-z7tq3FMLCSsCggEBANbCeomhRHqTE0cysMAGP38Em5Y9yvAE2WlcHRLcW+wK/iyi
-NJv5OG1883xG2d2nULLKNBikKBwUqeR4CiHDZB+NW7czqTi7FJ4KyUK+i4EKos3I
-sAH/0kmJJY5Vv5Ac1bLgVBTpFoqWxDXSYpdGZ4pgbDg5qUuKvtsOaNnP6KDltIm2
-2tlqF8FoTZw5bkkKgfXfiQLj3HWgmFZf7bDmByvXm7T23mhXPKopH2aAxBJrtqTz
-DKMHVYYgO++XE7kd9zSgzCdDgH4nNGc6fUiKbjcznvcEVUpF27XDzEzilIMRmLq5
-Oyt96dN5N0vE0+lboWlgk/dYNyCpynGNnwafkNECggEBAOCfPyonm45UmhyrcWEe
-VGvSF37f84xmWBZzq6lrHkUweXZLwkxYCecUbxDEQmAAhh2z5srlJH+9843SyZVH
-/QjnjuOkvLX59MgGaFxUgg4piRbGrY6KBOJvbyDu8/mGc9xb3rWcP3ZuLv3o9qKV
-eBTLPELlh5ytLSJvXE53yrG1EewyTttBTZtnqVGnBNSBo3ERWzImr1nhf/PAlEF4
-u4Cwd53TzL8JAqQomZ86z/Y+gEfMvptbX0kjq52cfwt2dgzxZS3YoOZhIzkW4YvK
-1R6fL60JZfqNj5aIfdpJ11Sh3t/MCUpuvD619VwZzFDzJwdl/MSNzR0TMDJnzrcS
-COcCggEAGItBNEzx4h9v6uE4xX0OkixpjYNnpmb0Ro4vNVsDMOtQP52CWvfpMRWY
-K3XPwY9279yYPVSW3qNGncSTcZKsW2CcxHruxKeqlbgXAFBKdSXJdpqXxbbWNEow
-LYYkhX+jZOZwAmPID9a3VpuzfwC7YEWItff2F0MOf29ccKoL9SGQ+cvBPWs16jfO
-Wv8/zdVQrmKwkp2yy51TPRJHDOcWwy6Vc8RxMH7/nmsZiA63Uo/3ndKiW8ZS6sHa
-9s9v9+Yq4XF2auMkoLQF/StBhKIbOYI+DZoYYEJ/gnzG5Dcm6mhpOelIl0GXENqn
-O9v69hkZwuZ3T3fLyTf54Rd/43/zSwKCAQABWDIVhfdUaCENdDCnE0GNpTVX1jkZ
-rNW0V0F6FZYpi2S7Goj5for2wE2MaQc7Umz/6NFXwd6Z9FQgd06NxGeIYJ9ynCKg
-v3kdjEeCBC341bHwecJuAsXYwZVO+5wJW5KS55S0Xnv6ncW7uesd17Nrh/O6Yj10
-PCX8d38zhXjBuqnRTUPcjo6Gu3zpTPGInBTlM1sk8awbDYHczQWTyoYTvYDzfzSf
-mr/NpY2xl4F0O2Wb4uChgCRbjvByuIvjehyAS0CWOh5btm9ZBt4oqLy7+Flc+jAg
-NIjTXmZUlOKefh4MBilqUU3KQG8GBHjzYl2TkFVGLNYGNA0U8VG8SUJq
------END RSA PRIVATE KEY-----
-",
-  mode    => 0600,
-  require => File['/var/lib/puppet/.ssh']
-}
-file {'/etc/puppet/cookbook':
-  ensure => 'directory',
+  owner => '1100',
+  group => '1100',
+  mode  => '0755',
 }
 
-exec {'download git.example.com host key': 
-  command => 'sudo -u puppet ssh-keyscan git.example.com >> /var/lib/puppet/.ssh/known_hosts',
-  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-  unless  => 'grep git.example.com /var/lib/puppet/.ssh/known_hosts',
+file {'githome':
+  ensure => 'directory',
+  path   => '/usr/local/git',
 }
-file {'/var/lib/puppet/.ssh/known_hosts':
-  owner   => 'puppet',
-  group   => 'puppet',
+
+group {'git':
+  gid => '1100',
+}
+
+user {'git':
+  uid     => '1100',
+  gid     => '1100',
+  home    => '/usr/local/git',
+  comment => 'GIT User',
+}
+
+file {'git-ssh':
+  ensure => 'directory', 
+  path   => '/usr/local/git/.ssh',
+  mode   => '0700',
+}
+
+$git_id_rsa = @(GIT)
+  -----BEGIN RSA PRIVATE KEY-----
+  MIIJJgIBAAKCAgBtapnTg9fQJg4xpCUw1ofQlRNN5WTFWEkIZYH1IXEvEWUBonAO
+  ahvO+1VRb5FSLmblddPO7um7oK8IYEzYF7jEVm2tgh2kl2TPYUPWybO8QVcYaMOa
+  L5kypR6x3RkWVaj/rwlyrsPCco4GpyYuZLpsNnk5mGynsgPqHoNdFC5fj2BeO88Q
+  v3A/pckJyT0xhz1WJE8uOgX4fd/IuYjTomCNAu2cwdbfr+19v3Wo7CWy9NxwzQDw
+  Deu50WoPj+7i8hKlnE1oyfWRLvmn6+GakCk7KSCzakfW7YQIqXWejONkHrdgXvxq
+  aeExHjaeQ0hmqcG30edeBOAdjjvOwHIXQObYErII7mF6vEq0yGnhjQ0vKDg+gxUW
+  N1yAthuHyBYxspZv7rB2PDXzEDWY4lHTEbuCarB/0TQ76XDS8eSd4uJfz4MfjbpL
+  YjZFuoJA5jcXgW9/9IafmmxV5b/7cbGO6ikqH3U+zF+KXXJkKXeRwJlba1QDj+Kl
+  vZ10D0cV0qpS9O3/hE0uestOCMDHemQYprrzXQW0av0uOPbdkjrcL/PaE0A1ta7b
+  lJdGTbmeblN5g9NlPq+aVL1ldUc3DTB2mfmPFPdej+daICE0pNaKgmkc6mKBzRin
+  kPcvBPaZMTX3x4rH1A8QgT5Bj5BaP6xgTrlT9Ul9PhOSB1cSaVvCXXapEwIDAQAB
+  AoICAFKxMBSVHfOn7y5BOgJRAE/5ep9ycNhcwsVfwfn7DkleQhIcK8gZFW82r+Je
+  CVwsyYM9q5hHI8qzw9T2wtzGANQR5c4t+i08Zen9PoID6pyqXSKLetj6hDOsayhN
+  SvqAPrsOFHvh7Pz2CrSoesixQ9soyDFpKyUJ+r9j1C/yYqHCADj3xqSHqlPP/cIx
+  ntfht890nYr4Oy70Ir7Wybv4ijPINMA2FyjzEu/oVQoXft5jBH8T0FOckbzbseaV
+  Bxvpi4IyQKaji73v3Kq2MMjZju+xQ4+VETo0Q4cwWz46iFhQhpKE9wuNfa3bgbfW
+  bc95GHjOYKfD04VCy5yUASXl3Q/8LzsP8sClDyr+Zk9tBztx7FlEj/pZQXqz3K31
+  qrqMgyHD/lOtubgOlPRjG0L0n8PC0+zY4F86018ea/S9N2wy/3MCaodtSNQHPM8x
+  SWQZiNEl2WNHETJybai7Bc5fBzzbeVy6NHAAt8i0v4TkTi7UVeqTueBeqnm/Bz2b
+  d3Yo3pAC/7rYYbykpsByPXkOSJ3x7hA+5XveB9uGDwf63sFkz6AO0TPKd9rdmk5f
+  RvkS1cTau0V3ZpvzU5gxXPvGqDv2ROM1SSdrAXe2fJlwTR2mgn4vOeIw+inLxF4a
+  4kZzUuK/ZZiXuMIsRADuWKTI9ygmNNO0nmvfSl5CXf0uFbNBAoIBAQD1nt3A5z3E
+  hQp9MGG59TqFWohOKHnFL3wIZl05KRXtast13Qtppi0CnreoR7FQnoEclehFP71d
+  wtxBd3JOhsVksiijYmrx5B4rZwr8QdL6T7O7j32JLtCbRHVUjJjvx3Tt/Brluuok
+  l+WedqFn4tzFiGpDgUnZXiWu0WNstU5EdSTffWvoZPu2WGlMli6h0wEW6NTZyX3z
+  tosF4+pwazJaIXscXAFFfOCeFeMpvnkVUamuvss/Sh5VbmxZvPr5VX4FffusRx/c
+  CCcTXbrjFJM9siXyFRjekH42anPz1S0cogbK31bWAnWxu2sOkOsQZNWA47JC00ck
+  PNlY00ZKWw4RAoIBAHIKRbJc3gBSanQnLsjbO420+iXzGSBmPrxJSdjdSp9GrDgS
+  xoX0ukG+2T8A0/0IvgR0AprIV/38H42Vqk6h/fOhadrm5GtmK7whc7gmjFM0yfx6
+  1UfOypqFzaoJh3WFIkRzfBmUIG2S/BhyLI+7aMrMa8vF8w8+3Ao41A1sjyy4m/N9
+  NU20ySVKAH+VBaGJWYoUYrW8H/n6N9O5qgI8WuuSC75QWRiY2EwOOaRWr/p9BUDc
+  VHidR/ZIj0x0r70xLRedjwnPPMbw8OnyNgx6P385rJcb0l6wXBLK5zCJkJ27lIsS
+  ORlECR6Kooo3QlW+eaa0HjrDFhDP6UyXP5y2MOMCggEAPwMTpsJsMTdkWnuIBrnn
+  2oBASbTOkIy/CDSio+BrdK4aJlGf9Omh0HNBcJV74s/XFatLnEBe52fvM4heNdET
+  YOLn1AqSnx3Ds6TKx65nXroFxQ9pyEDPWZX1d8xeC92inM85Y0hRezPQv5d1jv4/
+  XxnBETJg7LffePrUw/W7b4qR/BfkEAmgHcaOtQwuSGumcucme/zaKCTjg45V/aMy
+  lrbSK5Na7CW+RjAdR/9KdvKtUlw2LjVolMgwWdFGnaKWIviQEUFp5fvdcnVwVYNY
+  xtImM/W/nJF7H6CtwJcm7JqQnYkwcNTOttr6+xCojVsg/uDrQYOMU7qck9IXzUmQ
+  0QKCAQBcj3vcdkX2Su5+kmMFwfOHZBH5BWQqiPJ4jp+ytoJf3tPfu1WI2obm4Ami
+  eoCViyf3o1WN3fiVfLL7jzOpGRBvU18AvpE9nrL2wswS9q7Exj7/f49m9jA51UqF
+  rbBVsZaLqpRaioD7juawzNLgYoN+vwbrYSKUCDg+z7tFngxjhv+FVOYTyR3N90+K
+  PiFQ6JNVYJBx41ys3+u6HKMhp21AcMy5TqvSLBS7PnyIp5e0Hw9JgsvJDhefjWtJ
+  6hfN3oEz/r1ff+ZbrqpLqD04SW0KTXyJNZq9IjXJRwj4u182t06sxkC0AiXItUuS
+  FaHwyGYi6WTz2u4o/Sfl84k9IB9zAoIBAQDsUaB8nzdORZzG3NKAKy0UXESbfEeE
+  Wm2yq0xw+btkazuuw2yYkHKxDMECVC6AN3inbm2QVaNCEnEk8R45Js8NPvTCXjy1
+  MUNZRHXNygudPpNfx7dqUnibSQs950gLY/WIGfAQhcWgfNCGVMny4Je2mCkRZYJs
+  3E4eyLziyid3K4ywbcVpT5F6645GRgHQDO0DhLR69Wm32qqGLx3sj5qSdTO0NXwP
+  4E66KRzIejNXAy3jsfw4rAzGDWihjR+rr5KkDt8WHK5k2tO6e2sHijybFtIryZnc
+  DERQtHSEKEw6dC/Zu4CRlreUtymYc7A2Ejis2JQpVkjaCzZ2BK4QMo3u
+  -----END RSA PRIVATE KEY-----
+  | GIT
+
+file {'git-id_rsa':
+  path    => '/usr/local/git/.ssh/id_rsa',
+  content => $git_id_rsa,
+  mode    => '0600',
+  require => File['git-ssh'],
+}
+
+exec {'download git.example.com host key':
+  command => 'ssh-keyscan git.example.com >>/usr/local/git/.ssh/known_hosts',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+  unless  => 'grep git.example.com /usr/local/git/.ssh/known_hosts',
+  require => File['git-ssh'],
+}
+file {'git-ssh-known_hosts':
+  path    => '/usr/local/git/.ssh/known_hosts',
   require => Exec['download git.example.com host key']
 }
 
+file {'cookbook':
+  ensure => 'directory',
+  path   => '/usr/local/git/cookbook'
+}
+
 exec {'create cookbook':
-  command => 'sudo -u puppet git clone git@git.example.com:repos/puppet.git /etc/puppet/cookbook',
+  command => 'sudo -u git git clone git@git.example.com:repos/puppet.git cookbook',
   path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-  require => [Package['git'],File['/var/lib/puppet/.ssh/id_rsa'],Exec['download git.example.com host key']],
-  unless  => 'test -f /etc/puppet/cookbook/.git/config',
+  cwd     => '/usr/local/git',
+  require => [ Package['git'],
+               File['git-id_rsa'],
+               Exec['download git.example.com host key']
+             ],
+  unless  => 'test -f /usr/local/git/cookbook/.git/config',
 }
