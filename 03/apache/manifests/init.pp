0d3083be1763c083af054438bc411d7aed2223e3
Thomas Uphill <thomas@narrabilis.com>
2018-03-20 21:33:47 -0700

    03

--- 03/apache/manifests/init.pp
+++ 03/apache/manifests/init.pp
@@ -1,13 +1,30 @@
 class apache {
   File {
-    owner        => 'apache',
-    group    => 'apache',
-    mode => 0644,
+    owner => 'apache',
+    group => 'apache',
+    mode  => 0644,
   }
+  package {'httpd': ensure => 'installed' }
+
+  $index = @(INDEX)
+    <html>
+      <body>
+        <h1><a href='cookbook.html'>Cookbook! </a></h1>
+      </body>
+    </html>
+    | INDEX
   file {'/var/www/html/index.html':
-    content => "<html><body><h1><a href='cookbook.html'>Cookbook!</a></h1></body></html>\n",
+    content => $index,
   }
+
+  $cookbook = @(COOKBOOK)
+    <html>
+      <body>
+        <h2>PacktPub</h2>
+      </body>
+    </html>
+    | COOKBOOK
   file {'/var/www/html/cookbook.html':
-    content => "<html><body><h2>PacktPub</h2></body></html>\n",
+    content => $cookbook
   }
 }
