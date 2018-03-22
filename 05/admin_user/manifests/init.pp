78fa4490d29f8c9be02440c9b53a7e79a5f0020e
Thomas Uphill <thomas@narrabilis.com>
2018-03-21 21:05:12 -0700

    05

--- 05/admin_user/manifests/init.pp
+++ 05/admin_user/manifests/init.pp
@@ -1,7 +1,11 @@
-define admin_user ($key, $keytype, $dotfiles = 'false') { 
+# define for admin_user
+define admin_user (
+  String                        $key,
+  Enum['ssh-rsa','ssh-ed25519'] $keytype,
+) {
   $username = $name
   user { $username:
-    ensure     => present,
+    ensure => present,
   }
   file { "/home/${username}/.ssh":
     ensure  => directory,
@@ -12,29 +16,20 @@ define admin_user ($key, $keytype, $dotfiles = 'false') {
   }
   ssh_authorized_key { "${username}_key":
     key     => $key,
-    type    => "$keytype",
+    type    => $keytype,
     user    => $username,
     require => File["/home/${username}/.ssh"],
   }
-  # dotfiles
-  if $dotfiles == 'false' {
-    # just create the directory
-    file { "/home/${username}":
-      ensure  => 'directory',
-      mode    => '0700',
-      owner   => $username,
-      group   => $username,
-      require => User["$username"]
-    }
-  } else {
-    # copy in all the files in the subdirectory
-    file { "/home/${username}":
-      recurse => true,
-      mode    => '0700',
-      owner   => $username,
-      group   => $username,
-      source  => "puppet:///modules/admin_user/${username}",
-      require => User["$username"]
-    }
+  # copy in all the files in the subdirectory
+  file { "/home/${username}":
+    recurse      => true,
+    mode         => '0700',
+    owner        => $username,
+    group        => $username,
+    source       => [
+      "puppet:///modules/admin_user/${username}",
+      'puppet:///modules/admin_user/base' ],
+    sourceselect => 'all',
+    require      => User[$username],
   }
 }
